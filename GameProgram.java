951f1692e6ec40274e92ddaa4c7e169c874dedc6
Seanna Qin <136040915+seannaqin@users.noreply.github.com>
2023-06-09 23:10:25 -0700

    GameProgram.java

--- GameProgram.java
+++ GameProgram.java
@@ -2,7 +2,7 @@
 import java.util.*;
 
 public class GameProgram {
-	public static final int shipNum = 2;
+	public static final int shipNum = 5;
 	public static void main(String[] args) {
 		Scanner console = new Scanner(System.in);
 		rules();
